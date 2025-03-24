0901eee8a7b6abeea30a820aa762c868f2f3798f
sosotakadze <takadzesoso9@gmail.com>
2025-03-24 16:37:35 +0400

    first commit

---
+++ day000/index.py
@@ -0,0 +1,61 @@
+from turtle import *
+ 
+ #we want to paint a house
+  
+  #step 1:  draw a square
+ 
+speed(30)
+ 
+width(7)
+ 
+color("red")
+begin_fill()
+forward(200)
+left(90)
+ 
+forward(200)
+left(90)
+ 
+forward(200)
+left(90)
+ 
+forward(200)
+left(90)
+end_fill()
+ 
+ 
+ # end of square
+ 
+ #drawing a door 
+ 
+width(7)
+ 
+forward(70) 
+color ("blue")
+begin_fill()
+left(90)
+  
+forward(120)
+right(90)
+ 
+forward(60)
+right(90)
+ 
+forward(120)
+end_fill()
+
+penup()
+goto(200,200)
+pendown()
+ 
+ 
+color ("green")
+begin_fill()
+right(150)
+ 
+forward(200)
+left(120)
+forward(200)
+end_fill()
+ 
+exitonclick()
