5e74f8036e3cde3ab907ce5d398c88acd0af8188
sosotakadze <takadzesoso9@gmail.com>
2025-06-22 15:52:26 +0400

    commit

--- day 06/classwork/classwork.py
+++ day 06/classwork/classwork.py
@@ -1,7 +1,5 @@
 name = input("enter your name: ")
-
 surname = input("enter your surname: ")
-
 age = input("enter your age: ")
 
 print("მე ვარ " + name + " " + surname + " მე ვარ " + age + " წლის")
---
+++ day 08/homework/homework.py
@@ -0,0 +1,17 @@
+# string  - ბრჭყალებში მოქცეული სიმბოლოები
+# integer - ყველა მთელი რიცხვი - უსასრულობიდან + უსასრულობამდე
+# float - ყველა ათწილადი
+# boolean - ლოგიკური ტიპის მნიშვნელობა , აქვს ორი მნიშვნელობა True და False
+
+
+name = "soso"
+age = 15
+height = 1.75
+fav_movie = "superbad"
+birthdey = 13.11
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(fav_movie))
+print(type(birthdey))
