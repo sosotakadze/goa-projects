b601060df34cc79c31772e91a14316aed2233cb3
sosotakadze <takadzesoso9@gmail.com>
2025-10-23 00:55:22 +0400

    commit

---
+++ group 70/day 50/cw/1.js
@@ -0,0 +1,72 @@
+// 1
+// console.log - არის იგივე print ფუნქცია. 
+// გამოაქვს ტერმინალში ნებისმიერი რამ მაგალითად console.log("hello world")
+
+
+// 2
+// 1 - let - ცვლადის შეცვლა შეგვიძლია, და let-ის დაწერა აღრ არის აუცილებელი.
+// 2 - const - უცვლადია, ჩანაცვლება არ ხდება მაგ: დაბადების წელი
+// 3 - var - აღრავინ იყენებს, ჩანაცვლება შეგვიძლია, და var-ის მიწერა აუცილებელია ჩანაცვლების დროს
+
+
+// 3
+let age1 = 15
+const name1 = "soso"
+const lastname1 = "takadze"
+
+// სახელი შეიძლება შეცვალო და სხვისი სახელი ჩაწერო მაგიტო დავწერე const-ით.
+
+
+// 4
+// 1 - let - ცვლადის შეცვლა შეგვიძლია, და let-ის დაწერა აღრ არის აუცილებელი.
+// 2 - const - უცვლადია, ჩანაცვლება არ ხდება. 
+
+
+// 5
+let age2 = 15
+age2 = 16
+console.log(age)
+
+
+// 6
+const name2 = "soso"
+console.log(name2)
+
+
+// 7
+let name3 = "soso"   
+let age3 = 15       
+
+// console.log("სახელი: " + name + ", ასაკი: " + age)
+
+
+
+// 8
+console.log("soso")
+console.log("15")
+console.log("apple")
+console.log("banna")
+console.log("BMW")
+
+
+// 9
+let age4 = 15
+let year = 2025
+const name4 = "soso"
+const lastname2 = "takadze"
+let otherage2 = 13
+
+
+// 10
+var age5 = 10
+var lastname3 = "takadze"
+var name5 = "soso"
+var car = bmw
+var fruit = apple
+
+var age6 = 12
+var age7 = 11
+
+let age8 = 13
+age8 = 15
+// let-ის მიწერა არ გინდა და var-ის მიწერა აუცილებელია.
---
+++ group 70/day 50/hw/2.js
@@ -0,0 +1,76 @@
+// 1
+// კომენტარი იწერება ორი დახრილი ხაზით - //
+
+
+// 2
+// console.log - არის იგივე print ფუნქცია. 
+// გამოაქვს ტერმინალში ნებისმიერი რამ მაგალითად console.log("hello world")
+
+
+// 3
+console.log("soso")
+console.log("15")
+console.log("apple")
+console.log("banna")
+console.log("BMW")
+console.log("messi")
+console.log("32")
+console.log("bannana")
+console.log("boat")
+console.log("lambo")
+
+
+// 4
+// სანამ ცვლადს შექმნი წინ უნდა დაუწერო let, const ან var.
+
+
+// 5
+let age1 = 15
+// let - ცვლადის შეცვლა შეგვიძლია, და let-ის დაწერა აღრ არის აუცილებელი.
+const year = 2025
+// const - უცვლადია, ჩანაცვლება არ ხდება. 
+var car = "bmw"
+// var - აღრავინ იყენებს, ჩანაცვლება შეგვიძლია, და var-ის მიწერა აუცილებელია ჩანაცვლების დროს.
+
+
+// 6
+let age2 = 21
+age2 = 17
+age2 = 33
+age2 = 101
+age2 = 69
+age2 = 18
+
+console.log(age1)
+
+
+// 7
+const born_year = 2009
+const my_name = "soso"
+const summer_first_month = "July"
+const my_lastname = "takadze"
+const  week_second_day = "tuesday"
+
+
+// 8
+var car1 = "BMW"
+var car2 = "MERCEDEC"
+var car3 = "PORSCHE"
+var car4 = "LAMBORGHINI"
+var car5 = "FERRARI"
+var car6 = "HONDA"
+var car8 = "SUBARU"
+var car9 = "JEEP"
+var car10 = "AUDI"
+
+
+// 9
+// იმიტომ რომ let-ის მიწერა არ გინდა და var-ის მიწერა აუცილებელია ცვლადის შეცვლის დროს.
+
+
+// 10
+var age3 = 91
+var lastname = "takadze"
+var name = "soso"
+var car = "AUDI"
+var fruit = "BANNANNA"
